156ca77ce0a75617d75fc6439076fb9c999740d1
Michael Bonner <michaelb@thinkful.com>
2019-02-27 15:46:03 -0500

    finished all endpoints

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+('Google', 'http://www.google.com', 'search engine', '5'),
+('Facebook', 'http://www.facebook.com', 'social media site', '4'),
+('Youtube', 'http://www.youtube.com', 'video site', '3');
---
+++ src/bookmarks/bookmarks-service.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const knex = require('knex')
+
+
+
+
+
+const BookmarksService = {
+
+    getBookmarkList(knex){
+        return knex.select('*').from('bookmarks');
+    },
+
+    insertBookmarks(knex, newItem){
+        return knex
+                .insert(newItem)
+                .into('bookmarks')
+                .returning('*')
+                .then(rows =>{
+                    return rows[0];
+                })
+    },
+
+    deleteBookmark(knex, id){
+        return knex
+                .where({id})
+                .delete()
+    },
+
+    updateBookmark(knex, id, newItemFields){
+        return knex
+                .where({id})
+                .update(newItemFields)
+    },
+
+    getById(knex, id){
+        return knex
+                .select('*')
+                .from('bookmarks')
+                .where({id})
+                .first()
+    }
+
+
+    
+};
+
+module.exports = BookmarksService;
